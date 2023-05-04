2bc7e0f63ec71d96ead6a11238aa6de584044f70
Roger <morenoroger001@gmail.com>
2023-05-04 02:12:46 -0400

    feat: Add Alert component
    - Pass child components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup2 from "./components/ListGroup";
 
 
@@ -8,7 +9,13 @@ function App() {
     console.log(item);
   }
 
-  return <div><ListGroup2 items={CITIES} heading="Cities" onSelectItem={handleSelectItem}/></div>;
+  // return <div><ListGroup2 items={CITIES} heading="Cities" onSelectItem={handleSelectItem}/></div>;
+  return (
+  <div>
+    <Alert>
+      Hello <span>World</span>
+    </Alert>
+  </div>)
 }
 
 export default App;
