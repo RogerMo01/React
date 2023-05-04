035712700030d1ffadba89e2bad93115cc1e7348
Roger <morenoroger001@gmail.com>
2023-05-04 01:40:01 -0400

    feat: Pass arguments via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import ListGroup2 from "./components/ListGroup";
 
+
 function App() {
-  return <div><ListGroup2 /><ListGroup2 /></div>;
+  const CITIES = ['New York', 'Paris', 'Manchester', 'Barcelona', 'Tokyo']
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
+  return <div><ListGroup2 items={CITIES} heading="Cities" onSelectItem={handleSelectItem}/></div>;
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,5 @@
 import { MouseEvent, useState } from "react";
 
-const CITIES = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
 // const ListGroup = ({ list = CITIES }) => (
 //   <>
 //     <h1>List</h1>
@@ -13,13 +11,19 @@ const CITIES = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 //   </>
 // );
 
-const message = CITIES.length === 0 ? <p>List is empty</p> : null;
-const getMessage = () => {
-  return CITIES.length === 0 ? <p>List is empty</p> : null;
-}; // a function is better when we pass parameters from the other function
+interface Props {
+  items: string[]
+  heading: string
+  onSelectItem: (item: string) => void
+}
+
+// const message = CITIES.length === 0 ? <p>List is empty</p> : null;
+// const getMessage = () => {
+//   return CITIES.length === 0 ? <p>List is empty</p> : null;
+// }; // a function is better when we pass parameters from the other function
 
 // Event Handler
-const handleClick = (event : MouseEvent) => console.log(event);
+const handleClick = (event: MouseEvent) => console.log(event);
 
 // let selectedIndex = -1;
 // I want this variable to be a state of this component, and its value will change over time
@@ -28,25 +32,31 @@ const handleClick = (event : MouseEvent) => console.log(event);
 // arr[0] // variable (selectedIndex)
 // arr[1] // updater function
 
-function ListGroup2() {
-  
-const [selectedIndex, setSelectedIndex] = useState(-1);
+function ListGroup2({items, heading, onSelectItem}: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/* {message} */}
       {/* {getMessage()} */}
-      {CITIES.length === 0 && <p>List is empty</p>}
+      {items.length === 0 && <p>List is empty</p>}
 
       <ul className="list-group">
-        {CITIES.map((item, index) => (
+        {items.map((item, index) => (
           <li
-            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
             // onClick={() => console.log("Clicked " + item + " at index " + index)}
             // onClick={handleClick}
-            onClick={() => {setSelectedIndex(index);} }
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
